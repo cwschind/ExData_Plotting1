31df6a6df19489f78b1a77e685cdcaee34855b74
cwschind <cwschind@verizon.net>
2014-06-04 19:38:29 -0400

    Plot1 R code

--- plot1.R
+++ plot1.R
@@ -1,13 +1,20 @@
 plot1 <- function() {
+  
+## Open file to save graph  
   png(file = "plot1.png")
-  x <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", nrows = 100000)
+  
+## Read the data table  
+  x <- read.table("household_power_consumption.txt", header = TRUE, 
+                  sep = ";", na.strings = "?", nrows = 100000)
+## Choose the two dates for graphing  
   y <- subset(x, Date == "1/2/2007" | Date == "2/2/2007")
+## Set the Date column to include both date and time and convert to R time format
   y$Date <- paste(y$Date,y$Time)
   y$Date <- strptime(as.character(y$Date),format="%d/%m/%Y %H:%M:%S")
+## Make the histogram with appropriate labels and color
   with(y, hist(Global_active_power, main = "Global Active Power", 
                xlab = "Global Active Power (killowatts)", col = "red"))
-  ##dev.copy(png, file = "plot1.png")
+## Close file
   dev.off()
-  ##print(head(y))
-  ##print(tail(y))
+
 }
